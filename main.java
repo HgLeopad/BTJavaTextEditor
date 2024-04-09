9550403ac30a4d2cd17908c65d18d6c59fee2da7
HgLeopad <153881625+HgLeopad@users.noreply.github.com>
2024-04-09 13:15:42 +0700

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-import controller.texteditorController;
-import view.texteditorView;
-
-public class main {
-    public static void main(String[] args) {
-        new texteditorController(new texteditorView());
-    }
-}
